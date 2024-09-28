62ce5e8334ac0f547244d1843591b6bdbaa62c0f
shivyam <144865023+shivyam@users.noreply.github.com>
2024-09-28 05:36:15 -0400

    added tabs + icons for frontend

--- centsible-frontend/src/App.tsx
+++ centsible-frontend/src/App.tsx
@@ -3,52 +3,25 @@ import './App.css'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
 
 function App() {
 
   return(
   <Tabs>
     <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
+      <Tab>Summary</Tab>
+      <Tab>ChatBot</Tab>
+      <Tab>Resources</Tab>
     </TabList>
 
     <TabPanel>
-      <h2>Any content 1</h2>
+      <h2>Summary: </h2>
     </TabPanel>
     <TabPanel>
-      <h2>Any content 2</h2>
+      <h2>ChatBot: </h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Additional Resources: </h2>
     </TabPanel>
   </Tabs>
   )
