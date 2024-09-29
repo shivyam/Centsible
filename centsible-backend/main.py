77d1b772f39e6dddc5dcde65ff00c0d8b8ce0c6b
shivyam <144865023+shivyam@users.noreply.github.com>
2024-09-28 20:08:34 -0400

    merging

--- centsible-backend/main.py
+++ centsible-backend/main.py
@@ -4,9 +4,4 @@ from routers.chat import router as chat_router  # Importing the router
 app = FastAPI()
 
 # Include the router directly
-<<<<<<< HEAD
-app.include_router(summary_router)
-
-=======
 app.include_router(chat_router)
->>>>>>> 63b6bec43958468316b1f438678c91617f7a6d02
